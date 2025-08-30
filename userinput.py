41c911ce05aa1ea381527417c06cc93e69a1d573
alesierraalta <alejandrosa2000@gmail.com>
2025-08-30 19:26:55 -0400

    Priority push: current app optimizations and UX improvements

--- userinput.py
+++ userinput.py
@@ -1,4 +1,4 @@
 # userinput.py
 # (save exactly this file at the project root)
-user_input = "debes de ponerlo en la seccion de contacto y usar userinput"
+user_input = input("prompt: ")
 print(user_input)
